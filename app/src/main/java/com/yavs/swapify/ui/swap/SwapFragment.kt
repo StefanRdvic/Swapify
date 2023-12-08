5cb98590a94b0f29eec4203274b1527cabb7f4e5
Yan2708 <74718439+Yan2708@users.noreply.github.com>
2023-12-08 15:36:41 +0100

    playlist

--- app/src/main/java/com/yavs/swapify/ui/swap/SwapFragment.kt
+++ app/src/main/java/com/yavs/swapify/ui/swap/SwapFragment.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.yavs.swapify.R
 import com.yavs.swapify.auth.TokenManager
-import com.yavs.swapify.utils.Platform
 
 
 class SwapFragment : Fragment() {
@@ -23,16 +20,9 @@ class SwapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setSpinner(view)
-
     }
 
-    fun setSpinner(view: View){
-        viewModel.setTokens(requireContext())
-        val dropdown: Spinner = view.findViewById(R.id.platformSelect)
-        val adapter: ArrayAdapter<Platform> = ArrayAdapter<Platform>(view.context, R.layout.platform_item, viewModel.availablePlateforms)
-        dropdown.setAdapter(adapter)
-    }
+
 
 
 }
